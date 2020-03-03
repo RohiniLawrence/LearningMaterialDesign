69a56789cfd1a70024a1c56e4bebc377e5477f23
rohinilawrence <rohinilawrence@gmail.com>
2020-03-03 17:33:14 -0500

    added theme compatibility for lower API versions

--- app/src/main/java/com/exercises/learningmaterialdesign/MainActivity.java
+++ app/src/main/java/com/exercises/learningmaterialdesign/MainActivity.java
@@ -2,6 +2,7 @@ package com.exercises.learningmaterialdesign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+//call material design API here
+        }else{
+
+        }
+
     }
 }
